575d2577e06c461aab6c2e7474b2ffa2d58e4f5c
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2022-09-03 18:23:00 +0100

    Define more colors

--- src/colors.cpp
+++ src/colors.cpp
@@ -9,6 +9,7 @@
 #define BABYLON_PINK 2
 #define BABYLON_GREEN 3
 #define BABYLON_BLUE 4
+#define BABYLON_RED 5
 
 #define HACKER_PAIR_0 121
 #define HACKER_PAIR_1 131
@@ -18,14 +19,20 @@
 
 */
 
-void initializeColors()
+void initializeBasicColors()
 {
     if (has_colors())
     {
         start_color();
+        init_color(BABYLON_BLACK, 0, 0, 0);
         init_color(BABYLON_PINK, 999, 0, 500);
-        init_color(BABYLON_GREEN, 0, 255, 0);
+        init_color(BABYLON_RED, 999, 0, 0);
+        init_color(BABYLON_GREEN, 0, 999, 0);
+        init_color(BABYLON_BLUE, 0, 0, 999);
 
-        init_pair(HACKER_PAIR_0, OWN_PINK, BABYLON_BLACK);
+        init_pair(HACKER_PAIR_0, BABYLON_PINK, BABYLON_BLACK);
+        init_pair(HACKER_PAIR_1, BABYLON_RED, BABYLON_BLACK);
+        init_pair(HACKER_PAIR_2, BABYLON_GREEN, BABYLON_BLACK);
+        init_pair(HACKER_PAIR_3, BABYLON_BLUE, BABYLON_BLACK);
     }
 }
--- src/colors.hpp
+++ src/colors.hpp
@@ -4,15 +4,17 @@
 #define BABYLON_WHITE 0
 #define BABYLON_BLACK 1
 #define BABYLON_PINK 2
-#define BABYLON_GREEN 3
-#define BAB_BLUE 4
+#define BABYLON_RED 3
+#define BABYLON_GREEN 4
+#define BABYLON_BLUE 5
 
-#define HACKER_PAIR_0 121
-#define HACKER_PAIR_1 131
-#define HACKER_PAIR_2 131
-#define HACKER_PAIR_3 131
-#define HACKER_PAIR_4 131
+#define HACKER_PAIR_0 121 // Pink Black
+#define HACKER_PAIR_1 131 // Red Black
+#define HACKER_PAIR_2 141 // Green Black
+#define HACKER_PAIR_3 151 // Blue Black
 
-void initializeColors();
+void initializeBasicColors();
+
+void initializeAdvancedColors();
 
 #endif
